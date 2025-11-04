d45d8c3a72bff6f01a0de41ae57af27a2e8bac64
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-05 02:35:49 +0900

    Update In-game.js

--- In-game.js
+++ In-game.js
@@ -41,7 +41,7 @@ function eventCanvas(x, y) {
 function addSquare(x,y) {
     $.fillRect(_*x/Board.length+3,_*y/Board.length+3,_/Board.length-6,_/Board.length-6)
     //Solve = $.fillStyle
-    Qnumber.innerText = Solve
+    //Qnumber.innerText = Solve
 }
 
 function pickColor(latter) {
