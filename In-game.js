646f6f8cfd16ed9c4efa7bbe3ae5055741f52e38
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-05 19:45:57 +0900

    Update In-game.js

--- In-game.js
+++ In-game.js
@@ -27,11 +27,8 @@ function loadMap(Q) {
 function clickCanvas() {
             const x = event.clientX - $.canvas.offsetLeft;
             const y = event.clientY - $.canvas.offsetTop;
-alert ([
-    $.canvas.offsetWidth,
-    $.canvas.offsetHeight,
-    Canva.width,Canva.height,x,y,_
-])
+// _ -> $.canvas.offsetWidth:$.canvas.offsetHeight
+alert ([x,y])
     if (window.former != undefined) {
         const X = Math.round(x*Board.length/_-1/2)
         const Y = Math.round(y*Board.length/_-1/2)
