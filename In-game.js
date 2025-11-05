f14c1afa099c5e6f4b7d182173588219f395c95f
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-05 18:19:14 +0900

    Update In-game.js

--- In-game.js
+++ In-game.js
@@ -33,7 +33,7 @@ alert ([
     she.offsetTop+0,
     she.offsetWidth-0,
     she.offsetHeight+0,
-    she.offsetParent
+    JSON.stringify(she.offsetParent)
 ])
     if (window.former != undefined) {
         const X = Math.round(x*Board.length/_-1/2)
