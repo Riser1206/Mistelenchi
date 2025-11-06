cc3e65679f9e623a84e5271261511ad8200d01bc
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-06 17:25:29 +0900

    Update In-game.js

--- In-game.js
+++ In-game.js
@@ -5,7 +5,7 @@ const Y = "gold"
 const B = "blue"
 
 fetch('Levels/' + location.search + '.json').then(res => res.json()).then(data => loadMap(data)).catch($ => alert($))
-
+alert(location.search)
 function loadMap(Q) {
     Board = Q.Board
     Target = Q.Target
