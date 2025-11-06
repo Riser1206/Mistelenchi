848ebd4cb407ee40952fbb5860551c18aab1277d
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-06 19:27:33 +0900

    Update In-game.js

--- In-game.js
+++ In-game.js
@@ -52,8 +52,8 @@ function Check() {
    // Target 
     for (let i = 0; i < Board.length; i++) {
         for (let j = 0; j < 4; j++) {
-            conut += " vertical " + Solve[z].filter(item => item === triplet[j]).length
-            conut += " horizontal " + Trans[z].filter(item => item === triplet[j]).length
+            conut += " vertical " + Solve[i].filter(item => item === triplet[j]).length
+            conut += " horizontal " + Trans[i].filter(item => item === triplet[j]).length
         }
     }
         Qnumber.style="font-size:10;font-family: monospace;color:#ccc"
