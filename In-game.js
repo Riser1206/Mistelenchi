73a558232b2face5cf3aab26cff64cfed3e4c716
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-06 13:40:38 +0900

    Update In-game.js

--- In-game.js
+++ In-game.js
@@ -52,7 +52,7 @@ function Check() {
         //
     }
         Qnumber.style="font-size:5;font-family: monospace;color:#ccc"
-        Qnumber.innerText = conut + "\n\n\n" + Solve
+        Qnumber.innerText = conut + "\n\n\n" + Target
 }
 
 function addSquare(x,y) {
