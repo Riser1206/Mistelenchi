68131c5966e0ca86566a271733debe054093ff9d
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-08 16:08:20 +0900

    Update In-game.js

--- In-game.js
+++ In-game.js
@@ -107,8 +107,8 @@ function hintPaths(Object) {
     if ("Secondary" == Object.Color) $.strokeStyle = "#999"
     if ("Tertiary" == Object.Color) $.strokeStyle = "#666"
     $.stroke()
-    const Results = JSON.stringify([Group.includes(R),Group.includes(Y),Group.includes(B)])
-
+    const Union = [Group.includes(R),Group.includes(Y),Group.includes(B)]
+    const Results = JSON.stringify(Union)
     if (Object.Color == "Purple") return Results == "[true,false,true]"
     if (Object.Color == "Red") return Results == "[true,false,false]"
     if (Object.Color == "Orange") return Results == "[true,true,false]"
