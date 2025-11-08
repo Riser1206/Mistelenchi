2beeef6bcc47af53044ed09c2698f19495fef1d2
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-09 07:11:13 +0900

    Update In-game.js

--- In-game.js
+++ In-game.js
@@ -103,6 +103,7 @@ function hintPaths(Object) {
         $.lineTo(_*(x-.5)/Board.length, _*(y-.5)/Board.length)
     }
     $.strokeStyle = Object.Color
+    $.lineWidth = _/7/Board.length
     if ("Primary" == Object.Color) $.strokeStyle = "#ccc"
     if ("Secondary" == Object.Color) $.strokeStyle = "#999"
     if ("Tertiary" == Object.Color) $.strokeStyle = "#666"
