8589c9393a04c51e335f6f8315db04689fe2f88d
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-13 11:06:41 +0900

    Update In-game.js

--- In-game.js
+++ In-game.js
@@ -7,9 +7,7 @@ const R = "red"
 const G = "gold"
 const B = "blue"
 const Level = location.search.split('?')
-if (!Number.isInteger(Level[2] -= 0) || Level[2] < 1) spurn(Level[2],1)
-
-fetch(`Levels/${Level[1]}/${Level[2]}.json`).then(res => res.json()).then(data => loadMap(data)).catch($ => spurn($,9))
+if (!Number.isInteger(Level[2] -= 0) || Level[2] < 1) spurn(Level[2],1) else fetch(`Levels/${Level[1]}/${Level[2]}.json`).then(res => res.json()).then(data => loadMap(data)).catch($ => spurn($,9))
 
 function spurn(message, code) {
     alert(message + "\n" + code)
