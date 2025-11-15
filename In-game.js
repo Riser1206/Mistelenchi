3f213386d53a3cd4a593f90d85e0d6f72a9da9d5
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-15 15:19:25 +0900

    Update In-game.js

--- In-game.js
+++ In-game.js
@@ -70,7 +70,7 @@ function Count(Way, Language) {
     for (let i = 0; i < Board.length; i++) {
         for (let j = 0; j < 3; j++) {
             const n = Way[i].filter(item => item === triplet[j]).length
-            massage += i+1+Language+triplet[j]
+            if (Set != n) massage += i+1+Language+triplet[j]
             if (Set < n) massage+=" 초과\n"
             if (Set > n) massage+=" 부족\n"
         }
