c666f836a1c51086c9bed3d24c6abac6c21ab5e3
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-18 06:56:50 +0900

    Update In-game.js

--- In-game.js
+++ In-game.js
@@ -45,7 +45,6 @@ function Clear() {
             $.closePath()
         }
     }
-    Color = $.fillStyle = window.former != undefined ? former.background : "transparent"
     Given = 1
     for (const Path of Paths) hintPaths(Path)
 }
