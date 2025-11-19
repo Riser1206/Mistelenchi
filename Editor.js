28e365eb778955c9f300aed0ae65406b7710885c
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-19 18:06:49 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -13,6 +13,7 @@ function spurn(message, code) {
 }
 
 function loadMap(Stage,Set,Length) {
+    if (Set*3 == Length) _1.style.display="none"
     $.clearRect(0, 0, Canva.width, Canva.height)
     Qnumber.innerText = `Stage: ${Stage_=Stage}\nSet: ${Set_=Set}`
     pickColor(_0.style)
