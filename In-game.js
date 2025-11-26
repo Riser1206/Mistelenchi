e92e32805ee24d1b2ce2c6b5c0184532dc29bb58
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-26 14:51:31 +0900

    Update In-game.js

--- In-game.js
+++ In-game.js
@@ -23,8 +23,8 @@ function loadMap(Q) {
 }
 
 function clickCanvas() {
-    const X = Math.round((event.clientX-$.canvas.offsetLeft)*Board.length/$.canvas.offsetWidth-1/2)
-    const Y = Math.round((event.clientY-$.canvas.offsetTop)*Board.length/$.canvas.offsetHeight-1/2)
+    const X = Math.round((event.clientX-$.canvas.offsetLeft)*Board.length/$.canvas.offsetWidth-.5)
+    const Y = Math.round((event.clientY-$.canvas.offsetTop)*Board.length/$.canvas.offsetHeight-.5)
     if (!Board[Y][X]) addSquare(X,Y)
 }
 
