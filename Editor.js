1c2c6feb9748de069d2e91a165409e921e38b1c1
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-26 16:03:06 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -38,7 +38,7 @@ function clickCanvas() {
         isPathNum = Paths.length
     } else if (!window.isTest) fixSquare(X,Y); else if (!Board[Y][X]) addSquare(X,Y)
     for (const Path of Paths) drawPaths(Path)
-alert(isPathNum + "\n" + JSON.stringify(Paths))
+alert(isPathNum + "\n" + JSON.stringify(Board))
 }
 
 function addPath(Path) {
