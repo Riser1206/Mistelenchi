fd2da2328782323381d1c0f7aa2616a42d74a394
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-27 22:26:37 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -105,7 +105,7 @@ trianglePath.closePath();
                 $.lineWidth = _/37/Length_
                 $.beginPath()
                 $.arc(_*(Path.X+X-1.5)/Length_,_*(Path.Y+Y-1.5)/Length_,_/(X*Y-1 ? 9 : 5)/Length_,0,7)
-                $.fill()
+                $.fill(trianglePath)
                 $.stroke()
                 $.closePath()
             }
