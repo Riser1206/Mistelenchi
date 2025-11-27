b5d2a4c927333d753fa4604f22db392a9d0dfbbe
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-27 21:48:10 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -31,6 +31,7 @@ function clickCanvas() {
     const X = Math.round((event.clientX-$.canvas.offsetLeft)*Length_/$.canvas.offsetWidth-.5)
     const Y = Math.round((event.clientY-$.canvas.offsetTop)*Length_/$.canvas.offsetHeight-.5)
     if (isPathNum) {
+        if ()
         isPathNum = 0
     } else if (isPathNum == []) {
         const Path = {Color:Color,X:X+1,Y:Y+1,Array:[]}
