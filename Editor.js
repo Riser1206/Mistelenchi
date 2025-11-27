db0bc80b13c2640a4792701886387e414ee3a544
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-27 23:26:50 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -32,7 +32,7 @@ function clickCanvas() {
     const X = Math.round((event.clientX-$.canvas.offsetLeft)*Length_/$.canvas.offsetWidth-.5)
     const Y = Math.round((event.clientY-$.canvas.offsetTop)*Length_/$.canvas.offsetHeight-.5)
     if (isPathNum) {
-        if ($.isPointInPath(X,Y)) alert()
+        if ($.isPointInPath(event.clientX-$.canvas.offsetLeft,event.clientY-$.canvas.offsetTop)) alert()
         isPathNum = 0
     } else if (isPathNum == []) {
         const Path = {Color:Color,X:X+1,Y:Y+1,Array:[]}
