d30ad390af922764450f66fba0945dc716376968
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-27 22:58:38 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -104,6 +104,7 @@ function drawPaths(Path) {
                 Switch.closePath()
                 $.fill(Switch)
                 $.stroke(Switch)
+    alert(JSON.stringify({Z:Z,Switch:Switch}))
             }
         }
     }
