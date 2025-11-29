23e7c76d331efe1a890b67562c6fd16a216ad5b3
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-30 03:21:16 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -116,7 +116,7 @@ function pickColor(latter,Z) {
     if (window.former) former.background = latter.background
     Color = latter.background = latter.color
     former = latter
-    if (isPathNum) cease()
+    //if (isPathNum) cease()
 return alert()
     isPathNum = Z
     if (Board+Solve) drawSquare()
