d786c1ee40a8e3d5f6fef62eecea58c077a71565
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-11-29 20:03:25 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -33,7 +33,10 @@ function clickCanvas() {
     const Y = Math.round((event.clientY-$.canvas.offsetTop-5)*Length_/($.canvas.offsetHeight-10)-.5)
     if (X == -1 || Y == -1 || X == Length_ || Y == Length_) return
     if (isPathNum) {
-        for (let y = 0; y < 3; y++) for (let x = 0; x < 3; x++) if ($.isPointInPath(Switches[x][y],Math.round((event.clientX-$.canvas.offsetLeft-5)*_/($.canvas.offsetWidth-10)-.5),Math.round((event.clientY-$.canvas.offsetTop-5)*_/($.canvas.offsetHeight-10)-.5))) Paths[isPathNum-1].Array.push(4-Math.atan2(x-1,y-1)/Math.PI*4)
+        for (let y = 0; y < 3; y++) for (let x = 0; x < 3; x++) if ($.isPointInPath(Switches[x][y],Math.round((event.clientX-$.canvas.offsetLeft-5)*_/($.canvas.offsetWidth-10)-.5),Math.round((event.clientY-$.canvas.offsetTop-5)*_/($.canvas.offsetHeight-10)-.5))) {
+            if (X*Y-1)
+            Paths[isPathNum-1].Array.push(4-Math.atan2(x-1,y-1)/Math.PI*4)
+        }
      alert(JSON.stringify(Paths))
     } else if (isPathNum == []) {
         const Path = {Color:Color,X:X+1,Y:Y+1,Array:[]}
