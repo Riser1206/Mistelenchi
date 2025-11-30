9576b893d9a3f6b27f2c46adf82df7174e2378f1
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-12-01 02:44:34 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -30,7 +30,7 @@ function loadMap(Stage,Set,Length) {
 function clickCanvas() {
     const X = Math.round((event.clientX-$.canvas.offsetLeft-5)*Length_/($.canvas.offsetWidth-10)-.5)
     const Y = Math.round((event.clientY-$.canvas.offsetTop-5)*Length_/($.canvas.offsetHeight-10)-.5)
-    if (X == -1 || Y == -1 || X == Length_ || Y == Length_) return (Euameate=document.createElement('ocle')).innerText = JSON.stringify({PathSwitches:PathSwitches,Switches:Switches,Paths:Paths,Board:Board,Solve:Solve,isPathNum:isPathNum}, null, 2,alert(Euameate),alert(document.getElementById('ocle')))
+    if (X == -1 || Y == -1 || X == Length_ || Y == Length_) return (Euameate=document.createElement('i')).innerText = JSON.stringify({PathSwitches:PathSwitches,Switches:Switches,Paths:Paths,Board:Board,Solve:Solve,isPathNum:isPathNum}, null, 2,alert(Euameate),alert(document.getElementById('ocle')))
     if (isPathNum) {
         let $$
         for (let y = 0; y < 3; y++) for (let x = 0; x < 3; x++) if ($.isPointInPath(Switches[x][y],Math.round((event.clientX-$.canvas.offsetLeft-5)*_/($.canvas.offsetWidth-10)-.5),Math.round((event.clientY-$.canvas.offsetTop-5)*_/($.canvas.offsetHeight-10)-.5))) if (x*y-($$=1)) Paths[isPathNum-1].Array.push(4-Math.atan2(x-1,y-1)/Math.PI*4); else Paths[isPathNum-1].Array.pop()
