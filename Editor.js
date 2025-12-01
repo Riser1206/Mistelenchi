a60cf92fb7f3c05e0f647fb3e069dadbb2d85966
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-12-01 17:41:16 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -3,7 +3,8 @@ const $ = Canva.getContext("2d")
 $.lineCap = "round"
 $.lineJoin = "round"
 if (innerWidth > innerHeight) spurn("가로 화면입니다. 세로 화면으로 바꿔주세요."); else loadMap(``,1,3)
-(Euameate=document.createElement('textarea')).style.fontSize = 9
+const Euameate=document.createElement('textarea')
+Euameate.style.fontSize = 9
 alert(document.body.appendChild(Euameate))
 try{}catch(re){alert(re)}
 
