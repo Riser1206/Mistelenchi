b66fd017ae767c462b104b31214c7d87a4f5fceb
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-12-02 19:02:26 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -53,11 +53,9 @@ function clickCanvas() {
 function select() {
     $.lineWidth = _/7/Length_
     PathSwitches.forEach((PathSwitch, i) => {
-        if ($.isPointInStroke(PathSwitch,Math.round((event.clientX-$.canvas.offsetLeft-5)*_/($.canvas.offsetWidth-10)-.5),Math.round((event.clientY-$.canvas.offsetTop-5)*_/($.canvas.offsetHeight-10)-.5))) return Qnumber.innerText = i+1
-    })
-try{alert(PathSwitch)}catch(re){alert(re)}
-try{alert(i)}catch(re){alert(re)}
-  alert(Qnumber.innerText);
+        if ($.isPointInStroke(PathSwitch,Math.round((event.clientX-$.canvas.offsetLeft-5)*_/($.canvas.offsetWidth-10)-.5),Math.round((event.clientY-$.canvas.offsetTop-5)*_/($.canvas.offsetHeight-10)-.5))) Qnumber.innerText = i+1
+    })return Qnumber.innerText
+  alert();
 }
 
 function cease() {
