e0c2a9b7b207d51a7f86dec516e17d8fbde2041d
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-12-04 12:21:50 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -48,7 +48,7 @@ function clickCanvas() {
         }
     } else if (isPathNum == []) if (select()); else isPathNum = Paths.push({Color:Color,X:X+1,Y:Y+1,Array:[]}); else if (!window.isTest) Board[Y][X] = Solve[Y][X] = Color; else if (Board[Y][X] == "white") Solve[Y][X] = Color
     drawSquare()
-    try{cease($$$-1)}catch(re){alert(re)}
+    try{cease($$$-1)}catch(re){Qnumber.innerText = re+`\n`+$$$)}
     Euameate.value = JSON.stringify({isPathNum:isPathNum,Color:Color,PathSwitches:PathSwitches,Switches:Switches,Paths:Paths,Board:Board,Solve:Solve}, null, 2)
 }
 
