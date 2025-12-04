b3056300a74a40a0146eb72f51852370ca9657ac
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-12-04 11:00:45 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -47,7 +47,8 @@ function clickCanvas() {
         }
     } else if (isPathNum == []) if (select()); else isPathNum = Paths.push({Color:Color,X:X+1,Y:Y+1,Array:[]}); else if (!window.isTest) Board[Y][X] = Solve[Y][X] = Color; else if (Board[Y][X] == "white") Solve[Y][X] = Color
     drawSquare()
-    if ($$$+1) try{cease($$$)}catch(re){alert(re)}finally{Euameate.value = JSON.stringify({isPathNum:isPathNum,Color:Color,PathSwitches:PathSwitches,Switches:Switches,Paths:Paths,Board:Board,Solve:Solve}, null, 2)}
+    if ($$$+1) try{cease($$$)}catch(re){alert(re)}
+    Euameate.value = JSON.stringify({isPathNum:isPathNum,Color:Color,PathSwitches:PathSwitches,Switches:Switches,Paths:Paths,Board:Board,Solve:Solve}, null, 2)
 }
 
 function select() {
