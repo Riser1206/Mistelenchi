0d1c89e42e6063ce5a9fa5cdb30ad30f58ff988b
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-12-06 12:01:18 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -120,7 +120,7 @@ function drawPaths(Path,Z) {
     if (Results == "truetruetrue") $.strokeStyle = "#3f3f3f"
     $.lineWidth = _/15/Length_
     $.stroke(PathSwitch)
-    if (Paths[isPathNum-1] == Path) {
+    if (!Z && Paths[isPathNum-1] == Path) {
         for (let Y = 0; Y < 3; Y++) {
             for (let X = 0; X < 3; X++) {
                 const Z = $.createConicGradient(0, _*(x+X-1.5)/Length_, _*(y+Y-1.5)/Length_)
