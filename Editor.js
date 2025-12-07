05f25219700dd09d50f8a5cf6bb30e3c9fb62271
Official Riser 1206 <144973005+Riser1206@users.noreply.github.com>
2025-12-07 12:30:39 +0900

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -43,7 +43,7 @@ function clickCanvas() {
     if (isPathNum) {
         let Z
         for (let y = 0; y < 3; y++) for (let x = 0; x < 3; x++) if ($.isPointInPath(Switches[x][y],Math.round((event.clientX-$.canvas.offsetLeft-5)*_/($.canvas.offsetWidth-10)-.5),Math.round((event.clientY-$.canvas.offsetTop-5)*_/($.canvas.offsetHeight-10)-.5))) if (x*y-(Z=1)) Paths[isPathNum-1].Array.push(4-Math.atan2(x-1,y-1)/Math.PI*4); else Paths[isPathNum-1].Array.pop()
-        if (!Z) {Z[alert(Z)]
+        if (!Z) {
             select(cease(),isPathNum=0)
         }
     } else if (isPathNum == []) if (select()); else isPathNum = Paths.push({Color:Color,X:X+1,Y:Y+1,Array:[]}); else if (!window.isTest) Board[Y][X] = Solve[Y][X] = Color; else if (Board[Y][X] == "white") Solve[Y][X] = Color
